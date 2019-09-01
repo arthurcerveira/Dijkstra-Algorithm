92724fba57e603ac1e923fbd1983d01658893e8e
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-09-01 16:14:48 -0300

    Implementa o algoritmos de dijkstra e metodos auxiliares

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,20 +1,52 @@
 class Node(object):
-    def __init__(self):
-        self.linked_nodes = []
+    def __init__(self, index):
+        self.index = index
+
+        self.linked_nodes_index = []
         self.distance = float("inf")
         self.prev = -1
 
-        self.closed = False
+        self.open = True
+
+    def relax_adj(self, nodes):
+        for node_index, edge_value in self.linked_nodes_index:
+            distance = self.distance + edge_value
+
+            if distance < nodes[node_index].distance:
+                nodes[node_index].distance = distance
+                nodes[node_index].prev = self.index
 
 
 class Graph(object):
     def __init__(self):
         self.nodes = []
 
-        self.first_node = Node()
+        self.first_node = Node(0)
         self.first_node.distance = 0
-        self.first_node.closed = True
 
         self.nodes.append(self.first_node)
 
-    # def dijkstra_algorithm():
+    def dijkstra(self):
+        while self.open_nodes() is not None:
+            min_node = self.find_min_node()
+            min_node.open = False
+
+            min_node.relax_adj(self.nodes)
+
+    def open_nodes(self):
+        for node in self.nodes:
+            if node.open is True:
+                return node
+
+        return None
+
+    def find_min_node(self):
+        min_distance = float("inf")
+        min_node = None
+
+        for node in self.nodes:
+            if node.open and node.distance < min_distance:
+                min_node = node
+                min_distance = node.distance
+
+        return min_node

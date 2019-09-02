158085aae46878fea754be7fb85216863a437f31
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-09-02 17:13:11 -0300

    Implementa função para mostrar menor caminho obtido pelo algoritmo

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -16,6 +16,9 @@ class Node(object):
                 nodes[node_index].distance = distance
                 nodes[node_index].prev = self.index
 
+    def close(self):
+        self.open = False
+
 
 class Graph(object):
     def __init__(self):
@@ -39,7 +42,10 @@ class Graph(object):
             i = 0
 
             while i < data.__len__() and data[i] != '-1':
-                linked_nodes_index.append([int(data[i]), int(data[i + 1])])
+                node = int(data[i])
+                edge = int(data[i + 1])
+
+                linked_nodes_index.append([node, edge])
                 i += 2
 
             self.add_node(index, linked_nodes_index)
@@ -48,7 +54,7 @@ class Graph(object):
     def dijkstra(self):
         while self.open_nodes() is not None:
             min_node = self.find_min_node()
-            min_node.open = False
+            min_node.close()
 
             min_node.relax_adj(self.nodes)
 
@@ -70,6 +76,19 @@ class Graph(object):
 
         return min_node
 
+    def show_path(self, node_index):
+        path = []
+        node = self.nodes[node_index]
+
+        while node.index != 0:
+            path.append(node.index)
+            node = self.nodes[node.prev]
+
+        path.append(0)
+        path.reverse()
+
+        return path
+
 
 def main():
     graph = Graph()
@@ -78,6 +97,9 @@ def main():
         graph.add_nodes_from_file(nodes)
 
     graph.dijkstra()
+
+    for n in graph.nodes:
+        print("Best path to node " + n.index.__str__() + ": \n" + graph.show_path(n.index).__str__())
 
 
 if __name__ == "__main__":
